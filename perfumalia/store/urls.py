386f7c2e40c5173c5d3b51058758850b250e9b07
Pascual Gómez <pascual.gomez@offerup.com>
2024-05-21 22:37:07 -0500

    Add api from classmates

--- perfumalia/store/urls.py
+++ perfumalia/store/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('subscriptionplans/', SubscriptionPlansPageView.as_view(), name='subscription_plans'),
     path('cancelsubscription/<int:pk>/', CancelSubscriptionView.as_view(), name='cancel_subscription'),
     path("news", NewsPageView.as_view(), name='news'),
+    path("hotwheels", HotWheelsPageView.as_view(), name='hotwheels'),
 ]
